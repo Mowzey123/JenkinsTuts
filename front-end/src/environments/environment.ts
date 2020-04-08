aa2445a8410b123d2bb1ae89139b9a59f3f9c1db
root <cognativeinsights@gmail.com>
2020-04-08 16:04:45 +0100

    changed ports

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    baseurl : 'https://testapp.cognativeinsights.io/authservice/',
+    baseurl : 'http://178.128.203.102:800/authservice/',
     baseurl2 : 'https://testapp.cognativeinsights.io/transactionreportingapi/',
     baseurl3 : 'https://testapp.cognativeinsights.io/riskdashboardv1/',
     baseurl4 : 'https://testapp.cognativeinsights.io/burealdashboard/',
